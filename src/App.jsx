02ee195d8fe707139e0997a612b79a0f1ae08682
devJay123 <morest432@gmail.com>
2024-11-14 16:32:25 +0900

    Routes, Route 페이지 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
+import "./App.css";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Home from "./pages/Home";
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        {/* <div>hi</div> Routes 안에서는 Route 컴포넌트만 사용할 수 있다. */}
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="/*" element={<Diary />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;

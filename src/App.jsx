5a9caa09d32dfc59b8638f1454265738e05ea4f2
devJay123 <morest432@gmail.com>
2024-11-15 14:11:05 +0900

    util/js 파일로  여러 개의 이미지 추가, 폰트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
+import { getEmotionImage } from "./util/get-emotion.image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -18,6 +19,14 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} alt="emotion1" />
+        <img src={getEmotionImage(2)} alt="emotion2" />
+        <img src={getEmotionImage(3)} alt="emotion3" />
+        <img src={getEmotionImage(4)} alt="emotion4" />
+        <img src={getEmotionImage(5)} alt="emotion5" />
+      </div>
+
       <div>
         <Link to="/">Home</Link>
         <Link to="/new">New</Link>

3df679bffc12de5f0d5c8de94f632f8a06b5cd65
devJay123 <morest432@gmail.com>
2024-11-15 15:05:54 +0900

    Edit page 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,17 @@ import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import Edit from "./pages/Edit";
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-
-      <Button text={"default"} onClick={() => {}} />
-      <Button text={"positive"} type={"POSITIVE"} onClick={() => {}} />
-      <Button text={"negative"} type={"NEGATIVE"} onClick={() => {}} />
-
       <Routes>
-        {/* <div>hi</div> Routes 안에서는 Route 컴포넌트만 사용할 수 있다. */}
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
-        {/* url parameter 동적으로 받기 */}
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="/*" element={<Notfound />} />
-        {/* `*`는 와일드 카드. switch문의 default와 비슷함 */}
       </Routes>
     </>
   );

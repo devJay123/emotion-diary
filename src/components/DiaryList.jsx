2055e34aba5aff696f0cbd19c8508fb2790a3d99
devJay123 <morest432@gmail.com>
2024-11-18 10:06:25 +0900

    각 페이지 라우터 nav, home 화면 최신순, 오래된순 구현

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,19 +1,44 @@
 import "./DiaryList.css";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function DiaryList({ data }) {
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate - b.createdDate);
+      } else {
+        return Number(b.createdDate - a.createdDate);
+      }
+    }); // 새로운 배열을 정렬해서 반환하는 메서드 toSorted
+  };
+
+  const sortedData = getSortedData();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된순</option>
         </select>
-        <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
+        <Button
+          onClick={() => nav(`/new`)}
+          text={"새 일기 쓰기"}
+          type={"POSITIVE"}
+        />
       </div>
       <div className="list_wrapper">
-        {data.map((item) => (
+        {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>

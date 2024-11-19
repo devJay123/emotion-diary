181aaf3a1ac1e5023e86e093e69acd8efae2cd7e
devJay123 <morest432@gmail.com>
2024-11-19 13:03:18 +0900

    페이지 뒤로가기 nav(-1)

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,13 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
 
 export default function New() {
+  const nav = useNavigate();
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={
+          <Button
+            text={"< 뒤로 가기"}
+            onClick={() => {
+              nav(-1);
+            }}
+          />
+        }
       />
       <Editor />
     </div>

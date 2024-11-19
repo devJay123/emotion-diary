00b872870d05f4aa7da92fada98e5cbceae35b50
devJay123 <morest432@gmail.com>
2024-11-19 17:55:22 +0900

    edit

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,6 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 export default function Edit() {
   const params = useParams();
-  return <div>{params.id}번 일기 수정페이지입니다.</div>;
+  const nav = useNavigate();
+
+  const onClickDelete = () => {
+    window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!");
+  };
+
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={
+          <Button
+            text={"< 뒤로가기"}
+            onClick={() => {
+              nav(-1);
+            }}
+          />
+        }
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 }
